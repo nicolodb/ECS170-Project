5596a18630cac0804f605a81c5580f2dd22ebcc4
vanessa <141893637+highnessa@users.noreply.github.com>
2023-09-12 18:09:45 -0700

    Add files via upload

---
+++ credentials.py
@@ -0,0 +1,6 @@
+import os
+
+os.environ['SPOTIFY_CLIENT_ID'] = '86eaae057832497da22690a8f4a64106'
+os.environ['SPOTIFY_CLIENT_SECRET'] = 'ab77eb3d3c0842888d98e4dff07f5049'
+os.environ["DATASET_PATH"] = '/Users/VANESSA/Documents/ecs170/project/spotify_music_dataset.csv'
+os.environ['DOWNLOAD_PATH'] = "/Users/VANESSA/Documents/ecs170/project/graphs/cleaned dataset/"
